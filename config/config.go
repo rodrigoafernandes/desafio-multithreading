104c73ed2570fc51915645edde8fd6a78dca3805
Rodrigo Fernandes <rodrigoafernandes@users.noreply.github.com>
2023-06-05 15:41:18 -0300

    Bugfix/config env vars (#2)
    * feat: add api call
    
    * feat: validates which api returns faster
    
    * fix: config app via env vars

---
+++ config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import "github.com/kelseyhightower/envconfig"
+
+type AppConfig struct {
+	ApiCepUrl    string `envconfig:"APICEP_URL"`
+	ViaCepUrl    string `envconfig:"VIACEP_URL"`
+	ApiTimeoutMS int    `envconfig:"API_TIMEOUT_MS"`
+}
+
+var AppCFG AppConfig
+
+func SetupApp() {
+	var appConfig AppConfig
+	if err := envconfig.Process("", &appConfig); err != nil {
+		panic(err)
+	}
+	AppCFG = appConfig
+}
